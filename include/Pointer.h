72ff1bfd0c26a864507fc912d010b260b9c2410c
ocha-- <140893815+ocha545@users.noreply.github.com>
2026-05-02 17:54:29 +0900

    WRL::ComPtr がC++/CLIで使えないことに伴って、自作ComPtrを作成しました。

---
+++ include/Pointer.h
@@ -0,0 +1,107 @@
+﻿#pragma once
+#include<memory>
+#include<utility>
+
+namespace Win32
+{
+	template<class Type>
+	class ComPtr
+	{
+	private:
+		Type* ptr;
+
+	public:
+		ComPtr()
+			:  ptr(nullptr)
+		{
+		}
+		ComPtr(decltype(nullptr) null)
+			: ptr(null)
+		{
+		}
+		explicit ComPtr(Type* ptr)
+			: ptr(ptr)
+		{
+			if (ptr) {
+				ptr->AddRef();
+			}
+		}
+		ComPtr(const ComPtr& other)
+			: ptr(other.ptr)
+		{
+			if (ptr) {
+				ptr->AddRef();
+			}
+		}
+		ComPtr(ComPtr&& other) noexcept
+			: ptr(other.ptr)
+		{
+			other.ptr = nullptr;
+		}
+		ComPtr<Type>& operator=(const ComPtr& other)
+		{
+			if (this == &other)
+			{
+				return *this;
+			}
+
+			if (other.ptr) {
+				other.ptr->AddRef();
+			}
+			if (ptr) {
+				ptr->Release();
+			}
+			ptr = other.ptr;
+
+			return *this;
+		}
+
+		ComPtr<Type>& operator=(ComPtr&& other) noexcept
+		{
+			if (this == &other)
+			{
+				return *this;
+			}
+
+			if (ptr) {
+				ptr->Release();
+			}
+			ptr = other.ptr;
+			other.ptr = nullptr;
+
+			return *this;
+		}
+
+		Type* operator->() noexcept
+		{
+			return ptr;
+		}
+		Type* operator->() const noexcept
+		{
+			return ptr;
+		}
+
+		~ComPtr()
+		{
+			Reset();
+		}
+
+		Type* Get() const noexcept
+		{
+			return ptr;
+		}
+
+		Type** GetAddressOf() noexcept
+		{
+			return &ptr;
+		}
+
+		void Reset()
+		{
+			if (ptr) {
+				ptr->Release();
+				ptr = nullptr;
+			}
+		}
+	};
+}
